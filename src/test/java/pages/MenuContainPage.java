e836ade2e779460ecab72558a42b07c17e0d6b86
Taygib <nfsmwm5@mail.ru>
2023-03-30 01:23:36 +0300

    testingPage

--- src/test/java/pages/MenuContainPage.java
+++ src/test/java/pages/MenuContainPage.java
@@ -1,13 +1,17 @@
 package pages;
 
 import com.codeborne.selenide.CollectionCondition;
+import data.ContentMenu;
+import org.junit.jupiter.params.provider.Arguments;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MenuContainPage {
+
     public void openOnlyPage() {
         open("ru/");
     }
--- src/test/java/testX5/X5GroupTestStart.java
+++ src/test/java/testX5/X5GroupTestStart.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import pages.WorkPage;
+//import pages.WorkPage;
 
 import java.util.List;
 import java.util.stream.Stream;
