051f0980257e6af63fcec81ec56b0adfd114b4fc
Taygib <nfsmwm5@mail.ru>
2023-08-06 14:14:07 +0300

    testingPageCorrected

--- src/test/java/testX5Group/TestBase.java
+++ src/test/java/testX5Group/TestBase.java
@@ -60,7 +60,9 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.addVideo();
+        if (isRemote) {
+            Attach.addVideo();
+        }
         Selenide.closeWebDriver();
     }
 }
